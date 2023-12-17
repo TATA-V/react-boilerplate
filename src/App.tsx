fc5a7f2f5f8657804165403bde6b428694acf7e8
Choi-hyeona <xhrl523@naver.com>
2023-12-17 17:46:43 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+
+import MainPage from "./pages/MainPage"
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+function MainPage() {
+  return (
+    <>
+      <h1 className="text-red-500 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default MainPage;
